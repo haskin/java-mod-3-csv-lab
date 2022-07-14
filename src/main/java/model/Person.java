18e6c05d68426c865313e311ca998a1f2dafb9dc
Adrian Khaskin <haskin.dev@gmail.com>
2022-07-14 16:00:39 -0400

    Created implementation

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -3,8 +3,12 @@ package model;
 public class Person {
     private String name;
 
-
     public Person(String name) {
         this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
+
 }
